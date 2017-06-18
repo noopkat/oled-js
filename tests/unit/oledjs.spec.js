4d38879ab92cda11bc0f13104be66d2b6711eae2
Suz Hinton <suz.hinton@gmail.com>
2017-06-18 16:12:52 -0400

    add drawPixel unit test 🎉

---
+++ tests/unit/oledjs.spec.js
@@ -0,0 +1,37 @@
+// test deps
+var test = require('tape');
+var td = require('testdouble');
+
+// require and stub oled.js
+var oledjs = require('../../oled.js');
+oledjs.prototype._setUpI2C = td.function();
+oledjs.prototype._transfer = td.function();
+
+// require and stub johnny-five
+td.replace('johnny-five');
+var five = require('johnny-five');
+var board = new five.Board();
+
+// import framebuffer snapshots
+var drawPixelBuffer = require('../buffers/drawPixel');
+
+test('drawPixel outputs correct buffer', function (t) {
+  t.plan(1);
+      
+  var options = {
+    width: 128,
+    height: 64, 
+    address: 0x3D
+  };
+
+  var oled = new oledjs(board, five, options);
+
+  oled.drawPixel([
+    [127, 0, 1],
+    [127, 31, 1],
+    [127, 16, 1],
+    [64, 16, 1]
+  ], false);
+
+  t.ok(oled.buffer.equals(Buffer.from(drawPixelBuffer)));
+});
